2e11df660bfa0d78f3593597d1f986d64e06dde1
Cristian <cristiantri022@gmail.com>
2021-02-17 20:40:48 -0500

    Se hace plantillas en el main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,51 @@
 #include<stdlib.h>
 using namespace std;
 
-void funcion_admin (){
+int menu ();
+int funcion_admin ();
+void funcion_profesor ();
+void funcion_estudiante ();
+
+int main()
+{
+    menu();
+
+    
+    return 0;
+}
+int funcion_admin (){
+    int opc;
     cout<<"Bienvenido a la zona de administrador."<<endl;
+    cout<<"Digite 1: Si quiere administrar estudiantes."<<endl;
+    cout<<"Digite 2: Si quiere administrar profesores."<<endl; 
+    cout<<"Digite 3: Si quiere administrar materias."<<endl;
+    cout<<"Digite 4: Si quiere administrar nota."<<endl;
+    cout<<"Digite 5: Si quiere volver al menu anterior."<<endl;
+    cout<<"Ingrese 0 para salir:"<<endl; 
+    cin>>opc;
+    switch (opc)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    case 5:
+        menu();
+        break;
+    case 0:
+        return 0;
+        break;
+    default:
+        break;
+    }
 }
 void funcion_profesor (){
     cout<<"Bienvenido a la zona de profesor."<<endl;
@@ -17,7 +60,7 @@ int menu (){
     cout<<"Ingrese 1 si es un administrador:"<<endl;
     cout<<"Ingrese 2 si es un profesor:"<<endl;
     cout<<"Ingrese 3 si es un estudiante:"<<endl; 
-     cout<<"Ingrese 4 para salir:"<<endl; 
+     cout<<"Ingrese 0 para salir:"<<endl; 
     cin>>opc;
     switch (opc)
     {
@@ -30,7 +73,7 @@ int menu (){
     case 3:
         funcion_estudiante();
         break;
-    case 4:
+    case 0:
         return 0;
         break;
     default:
@@ -38,12 +81,4 @@ int menu (){
         menu();
         break;
     }
-}
-
-int main()
-{
-    menu();
-
-    
-    return 0;
 }
