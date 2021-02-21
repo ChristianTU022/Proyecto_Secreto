4caf0f8f2e8f9f34fcdee40a10cc40a4c0a9383a
Cristian <cristiantri022@gmail.com>
2021-02-21 17:11:50 -0500

    Admin estudiante ademas de correccion de esteticas

--- main.cpp
+++ main.cpp
@@ -15,18 +15,191 @@ void leer_materias();
 fstream leer_archivo(string entidad);
 void editar_materia ();
 void eliminar_materia();
-
+int administrar_estudiante();
+void crear_estudiante();
+void leer_estudiante();
+void editar_estudiante();
+void eliminar_estudiante();
 struct materia
 {
     string codigo;
     string nom_materia;
 };
-
+struct estudiante
+{
+    string codigo;
+    string nom_estudiante;
+    string ape_estudiante;
+    string semestre;  
+};
 
 int main()
 {
     menu();
     return 0;
+}
+
+void eliminar_estudiante(){
+    string codigo, comparar;
+    string nom_estudiante, ape_estudiante, semestre;
+    string respuesta;
+    fstream leer = leer_archivo("estudiante.txt");
+    cout << "==========================================================================" << endl;
+        cout << "\t\tDigite el codigo del estudiante que desea eliminar."<<endl;
+        cin >> comparar;
+        cout <<"---------------------------------------------------------------------------" << endl;
+        while (!leer.eof())
+        {
+            leer >> codigo;
+            leer >> nom_estudiante;
+            leer >> ape_estudiante;
+            leer >> semestre;
+            if (comparar != codigo)
+            {
+                string registro = codigo + " " + nom_estudiante + " " + ape_estudiante + " " + semestre;
+                escribir_archivo("temp.txt", registro);
+            } 
+        }
+        cout <<"---------------------------------------------------------------------------" << endl;
+        cout << "\t\tDesee eliminar algun otro registro?"<<endl;
+        cin >> respuesta;
+        leer.close();
+        remove ("estudiante.txt");
+        rename ("temp.txt", "estudiante.txt");
+        if (respuesta == "si")
+        {
+            system("cls");
+            eliminar_estudiante();
+        } else if (respuesta == "no")
+        {
+            system("cls");
+            administrar_estudiante();
+        }
+        system("cls");
+        administrar_estudiante();
+}
+
+void editar_estudiante(){
+    string codigo, comparar;//,estudiante;
+    string nom_estudiante, ape_estudiante, semestre;
+    string cop_nom_estudiante, cop_ape_estudiante, cop_semestre;
+    fstream leer = leer_archivo("estudiante.txt");
+        cout << "==========================================================================" << endl;
+        cout << "\t\tDigite el codigo del estudiante que desea actualizar." << endl;
+        cin >> comparar;
+        cout <<"---------------------------------------------------------------------------" << endl;
+        cout << "\t\tQue nombre desea colocarle al estudiante. " << comparar << "?"<< endl;
+        cin >> nom_estudiante;
+        cout <<"---------------------------------------------------------------------------" << endl;
+        cout << "\t\tQue apellido desea al estudiante " << comparar << "?"<< endl;
+        cin >> ape_estudiante;
+        cout <<"---------------------------------------------------------------------------" << endl;
+        cout << "\t\tQue semestre quiere colocarle al estudiante. " << comparar << "?"<< endl;
+        cin >> semestre;
+        cout <<"---------------------------------------------------------------------------" << endl;
+        while (!leer.eof())
+        {
+            leer >> codigo;
+            leer >> nom_estudiante;
+            leer >> ape_estudiante;
+            leer >> semestre;
+
+            if (comparar == codigo)
+            {
+                //estudiante = nom_estudiante + " " + ape_estudiante + " " + semestre;
+                cop_nom_estudiante = nom_estudiante;
+                cop_ape_estudiante = ape_estudiante; 
+                cop_semestre = semestre;
+            }
+
+            string registro = codigo + " " + cop_nom_estudiante + " " + cop_ape_estudiante + " " + cop_semestre;
+            escribir_archivo("temp.txt", registro);
+        }
+        leer.close();
+        remove ("estudiante.txt");
+        rename ("temp.txt", "estudiante.txt");
+        cout << "==========================================================================" << endl;
+        system("pause");
+        administrar_estudiante();
+}
+
+void leer_estudiante(){
+    string codigo, estudiante, apellido, semestre;
+    fstream leer = leer_archivo("estudiante.txt");
+     cout << "==========================================================" << endl;
+     while (!leer.eof())
+        {
+            leer >> codigo;
+            leer >> estudiante;
+            leer >> apellido;
+            leer >> semestre;
+            cout <<  "\t\t" << codigo << "\t " << estudiante << "\t" << apellido << "\t"<< semestre <<"\t\t" <<endl;
+            cout << "-----------------------------------------------------" << endl;
+        }
+        leer.close();
+     cout << "==========================================================" << endl;
+    system("pause");
+    system("cls");
+    administrar_estudiante();   
+}
+
+void crear_estudiante(){
+    struct estudiante dat_estu;
+    cout << "================================================================" << endl;
+    cout << "\t\tDigite el codigo del estudiante: " << endl;
+    cin >> dat_estu.codigo;
+    cout <<"----------------------------------------------------------------" << endl;
+    cout << "\t\tDigite el nombre del estudiante: " << endl;
+    cin >> dat_estu.nom_estudiante;
+    cout << "----------------------------------------------------------------" << endl;
+    cout << "\t\tDigite el apellido del estudiante: " << endl;
+    cin >> dat_estu.ape_estudiante;
+    cout << "----------------------------------------------------------------" << endl;
+    cout << "\t\tDigite el semestre del estudiante: " << endl;
+    cin >> dat_estu.semestre;
+    cout << "----------------------------------------------------------------" << endl;
+    string registro = dat_estu.codigo + " " + dat_estu.nom_estudiante + " " + dat_estu.ape_estudiante + " Semestre_" + dat_estu.semestre;
+    escribir_archivo("estudiante.txt", registro);
+    cout << "================================================================" << endl;
+    system("pause");
+    system("cls");
+    administrar_estudiante();
+}
+
+int administrar_estudiante(){
+    int opc = opciones_base("estudiantes");
+    switch (opc)
+    {
+    case 1:
+        system("cls");
+        crear_estudiante();
+        break;
+    case 2:
+        system("cls");
+        leer_estudiante();
+        break;
+    case 3:
+        system("cls");
+        editar_estudiante();
+        break;
+    case 4:
+        system("cls");
+        eliminar_estudiante();
+        break;
+    case 5:
+        system("cls");
+        funcion_admin();
+        break;
+    case 0:
+        return 0;
+        break;
+    default:
+        cout << "\n\t\t-Error: La opcion digitada no es valida..." << endl;
+        system("pause");
+        system("cls");
+        administrar_estudiante();
+        break;
+    }   
 }
 
 void eliminar_materia (){
@@ -55,11 +228,14 @@ void eliminar_materia (){
         rename ("temp.txt", "materia.txt");
         if (respuesta == "si")
         {
+            system("cls");
             eliminar_materia();
         } else if (respuesta == "no")
         {
+            system("cls");
             administrar_materia();
         }
+        system("cls");
         administrar_materia();
 }
 
@@ -90,6 +266,7 @@ void editar_materia (){
         rename ("temp.txt", "materia.txt");
         cout << "==========================================================================" << endl;
         system("pause");
+        system("cls");
         administrar_materia();
 }
 
@@ -97,17 +274,18 @@ void leer_materias()
 {
     string codigo, materia;
     fstream leer = leer_archivo("materia.txt");
-     cout << "==========================================================" << endl;
+    cout << "================================================================" << endl;
      while (!leer.eof())
         {
             leer >> codigo;
             leer >> materia;
             cout <<  "\t\t" << codigo << "\t " << materia <<"\t\t" <<endl;
-            cout << "-----------------------------------------------------" << endl;
+            cout <<"----------------------------------------------------------------" << endl;
         }
         leer.close();
-     cout << "==========================================================" << endl;
+    cout << "================================================================" << endl;
     system("pause");
+    system("cls");
     administrar_materia();   
 }
 
@@ -139,6 +317,7 @@ void crear_materia()
     escribir_archivo("materia.txt", registro);
     cout << "================================================================" << endl;
     system("pause");
+    system("cls");
     administrar_materia();
 }
 
@@ -176,28 +355,37 @@ int opciones_base(string entidad)
 
 int administrar_materia()
 {
-    int opc = opciones_base("materias");
+    int opc = opciones_base("materias.");
     switch (opc)
     {
     case 1:
+        system("cls");
         crear_materia();
         break;
     case 2:
+        system("cls");
         leer_materias();
         break;
     case 3:
+        system("cls");
         editar_materia();
         break;
     case 4:
+        system("cls");
         eliminar_materia();
         break;
     case 5:
+        system("cls");
         funcion_admin();
         break;
     case 0:
         return 0;
         break;
     default:
+        cout << "\n\t\t-Error: La opcion digitada no es valida..." << endl;
+        system("pause");
+        system("cls");
+        administrar_materia();
         break;
     }
 }
@@ -219,24 +407,31 @@ int funcion_admin()
     switch (opc)
     {
     case 1:
-
+        system("cls");
+        administrar_estudiante();
         break;
     case 2:
 
         break;
     case 3:
+        system("cls");
         administrar_materia();
         break;
     case 4:
 
         break;
     case 5:
+        system("cls");
         menu();
         break;
     case 0:
         return 0;
         break;
     default:
+        cout << "\n\t\t-Error: La opcion digitada no es valida..." << endl;
+        system("pause");
+        system("cls");
+        funcion_admin();
         break;
     }
 }
@@ -268,19 +463,24 @@ int menu()
     switch (opc)
     {
     case 1:
+        system("cls");
         funcion_admin();
         break;
     case 2:
+        system("cls");
         funcion_profesor();
         break;
     case 3:
+        system("cls");
         funcion_estudiante();
         break;
     case 0:
         return 0;
         break;
     default:
-        cout << "La opcion digitada no es valida.";
+        cout << "\n\t\t-Error: La opcion digitada no es valida..." << endl;
+        system("pause");
+        system("cls");
         menu();
         break;
     }
